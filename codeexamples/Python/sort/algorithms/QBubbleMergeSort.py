46a367a7e8251bf7a8883c7ac2f527bdbbb2e73a
Harald Glab-Plhak <hglabplhak@gmail.com>
2025-10-20 22:11:32 +0200

    Initial commit for python

---
+++ codeexamples/Python/sort/algorithms/QBubbleMergeSort.py
@@ -0,0 +1,93 @@
+class QBubbleMergeSort:
+
+
+    def partition(self, arr, low, high):
+        # choose the pivot
+        pivot = arr[high]
+
+        # index of smaller element and indicates
+        # the right position of pivot found so far
+        i = low - 1
+
+        # traverse arr[low..high] and move all smaller
+        # elements to the left side. Elements from low to
+        # i are smaller after every iteration
+        for j in range(low, high):
+            if arr[j] < pivot:
+                i += 1
+                self.swap(arr, i, j)
+
+        # move pivot after smaller elements and
+        # return its position
+        self.swap(arr, i + 1, high)
+        return i + 1
+
+
+    # swap function
+    def swap(self, arr, i, j):
+        arr[i], arr[j] = arr[j], arr[i]
+
+
+    # the QuickSort function implementation
+    def quickSort(self, arr, low, high):
+        if low < high:
+            # pi is the partition return index of pivot
+            pi = self.partition(self, arr, low, high)
+
+            # recursion calls for smaller elements
+            # and greater or equals elements
+            self.quickSort(arr, low, pi - 1)
+            self.quickSort(arr, pi + 1, high)
+
+
+    def merge(self, arr, left, mid, right):
+        n1 = mid - left + 1
+        n2 = right - mid
+
+        # Create temp arrays
+        L = [0] * n1
+        R = [0] * n2
+
+        # Copy data to temp arrays L[] and R[]
+        for i in range(n1):
+            L[i] = arr[left + i]
+        for j in range(n2):
+            R[j] = arr[mid + 1 + j]
+
+        i = 0
+        j = 0
+        k = left
+
+        # Merge the temp arrays back
+        # into arr[left..right]
+        while i < n1 and j < n2:
+            if L[i] <= R[j]:
+                arr[k] = L[i]
+                i += 1
+            else:
+                arr[k] = R[j]
+                j += 1
+            k += 1
+
+        # Copy the remaining elements of L[],
+        # if there are any
+        while i < n1:
+            arr[k] = L[i]
+            i += 1
+            k += 1
+
+        # Copy the remaining elements of R[],
+        # if there are any
+        while j < n2:
+            arr[k] = R[j]
+            j += 1
+            k += 1
+
+
+    def mergeSort(self, arr, left, right):
+        if left < right:
+            mid = (left + right) // 2
+
+            self.mergeSort(arr, left, mid)
+            self.mergeSort(arr, mid + 1, right)
+            self.merge(arr, left, mid, right)
