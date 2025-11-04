d3bac50af774d3eed800a89443ba89e950253c65
Harald Glab-Plhak <hglabplhak@gmail.com>
2025-11-05 00:16:45 +0100

    added more stuff

--- codeexamples/Java/src/main/java/io/examples/complexcode/AvoidComplexCode.java
+++ codeexamples/Java/src/main/java/io/examples/complexcode/AvoidComplexCode.java
@@ -1,4 +1,118 @@
 package io.examples.complexcode;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.Consumer;
+
 public class AvoidComplexCode {
+
+    // do not make a instance!!!!
+    private AvoidComplexCode (){
+
+    }
+
+    public static void tooMuchNestedForCollect() {
+        AtomicReference<List<Integer>> nonsense;
+        nonsense = new AtomicReference<>(new ArrayList<>());
+        Consumer<AtomicReference<List<Integer>>> atomicReferenceConsumer = AvoidComplexCode::accept;
+        atomicReferenceConsumer.accept(nonsense);
+    }
+
+    public static void unrolledHmpf() {
+        AtomicReference<List<Integer>> nonsense = new AtomicReference<>(new ArrayList<>());
+        accept(nonsense);
+    }
+
+    private static void accept(AtomicReference<List<Integer>> nonsense1) {
+        {
+            List<String> strList = Arrays.asList("Hello this is Romeo".split(" "));
+            Integer counter = Integer.valueOf(0);
+            for (String part : strList)
+                if (part.trim().equals("I")) {
+                    nonsense1.get().add(counter++);
+                }
+        }
+        {
+            List<String> strList = Arrays.asList("here to talk".split(" "));
+            Integer counter = Integer.valueOf(0);
+            for (String part : strList) {
+                if (part.trim().equals("I")) {
+                    nonsense1.get().add(counter++);
+                }
+            }
+        }
+        {
+            List<String> strList = Arrays.asList("I am alive".split(" "));
+            Integer counter = Integer.valueOf(0);
+            for (String part : strList) {
+                if (part.trim().equals("I")) {
+                    nonsense1.get().add(counter++);
+                }
+            }
+        }
+        {
+            List<String> strList = Arrays.asList("am I crazy".split(" "));
+            Integer counter = Integer.valueOf(0);
+            for (String part : strList) {
+                if (part.trim().equals("I")) {
+                    nonsense1.get().add(counter++);
+                }
+            }
+        }
+        {
+            List<String> strList = Arrays.asList("the walking dead".split(" "));
+            Integer counter = Integer.valueOf(0);
+            for (String part : strList) {
+                if (part.trim().equals("I")) {
+                    nonsense1.get().add(counter++);
+                }
+            }
+        }
+        {
+            List<String> strList = Arrays.asList("in that case".split(" "));
+            Integer counter = Integer.valueOf(0);
+            for (String part : strList) {
+                if (part.trim().equals("I")) {
+                    nonsense1.get().add(counter++);
+                }
+            }
+        }
+        {
+            List<String> strList = Arrays.asList("a child plays".split(" "));
+            Integer counter = Integer.valueOf(0);
+            for (String part : strList) {
+                if (part.trim().equals("I")) {
+                    nonsense1.get().add(counter++);
+                }
+            }
+        }
+        {
+            List<String> strList = Arrays.asList("the evil hurricane".split(" "));
+            Integer counter = Integer.valueOf(0);
+            for (int i = 0, strListSize = strList.size(); i < strListSize; i++) {
+                String part = strList.get(i);
+                if (part.trim().equals("I")) {
+                    nonsense1.get().add(counter++);
+                }
+            }
+        }
+    }
+
+    public void abitStreamLined() {
+        List <String> input = Arrays.asList("the evil hurricane",
+                "a child plays",
+                "in that case",
+                "the walking dead",
+                "I am alive",
+                "Hello this is Romeo",
+                "here to talk" );
+        long result =  input.stream()
+                .sorted()
+                .map(e -> Arrays.stream(e.split(" "))
+                        .filter(e1 -> e1.trim().equals("I"))
+                        .findAny().orElse("null")).count();
+    }
 }
--- codeexamples/Java/src/test/java/io/examples/sort/QBubbleMergeTest.java
+++ codeexamples/Java/src/test/java/io/examples/sort/QBubbleMergeTest.java
@@ -13,6 +13,8 @@ import static io.examples.sort.QBubbleMerge.SortDirection.ASC;
 import static io.examples.sort.QBubbleMerge.SortDirection.DESC;
 import static io.examples.sort.SortData.*;
 import static java.util.Arrays.parallelSort;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
 
 public class QBubbleMergeTest {
 
@@ -43,7 +45,7 @@ public class QBubbleMergeTest {
             }
         });
         QBubbleMerge.quickSort(arrayToSort, ASC, 0, LAST_INDEX);
-
+        assertThat(Arrays.compare(arrayToSort, arrayToSortComp), is(0));
         arrayToSort = newArray();
         arrayToSortComp = newArray();
         Arrays.<Integer>parallelSort(arrayToSortComp, new Comparator<Integer>() {
@@ -53,12 +55,31 @@ public class QBubbleMergeTest {
             }
         });
         QBubbleMerge.quickSort(arrayToSort, DESC, 0, LAST_INDEX);
-
+        assertThat(Arrays.compare(arrayToSort, arrayToSortComp), is(0));
     }
 
     @Test
     public void testBubbleSortSimple() {
+        Integer[] arrayToSort = newArray();
+        Integer[] arrayToSortComp = newArray();
+        Arrays.<Integer>parallelSort(arrayToSortComp, new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o1.compareTo(o2);
+            }
+        });
+        QBubbleMerge.bubbleSort(arrayToSort, ASC, LAST_INDEX);
 
+        arrayToSort = newArray();
+        arrayToSortComp = newArray();
+        Arrays.<Integer>parallelSort(arrayToSortComp, new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o2.compareTo(o1);
+            }
+        });
+        QBubbleMerge.bubbleSort(arrayToSort, DESC, LAST_INDEX);
+        assertThat(Arrays.compare(arrayToSort, arrayToSortComp), is(0));
     }
 
     @Test
