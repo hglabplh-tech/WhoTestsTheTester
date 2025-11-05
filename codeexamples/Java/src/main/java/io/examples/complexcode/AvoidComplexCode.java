409ee7008bf37dc9a6acd2db7307ea386cc07435
Harald Glab-Plhak <hglabplhak@gmail.com>
2025-11-05 22:32:20 +0100

    added more code

--- codeexamples/Java/src/main/java/io/examples/complexcode/AvoidComplexCode.java
+++ codeexamples/Java/src/main/java/io/examples/complexcode/AvoidComplexCode.java
@@ -1,118 +1,145 @@
 package io.examples.complexcode;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.Consumer;
+import java.util.stream.Collectors;
 
 public class AvoidComplexCode {
 
     // do not make a instance!!!!
-    private AvoidComplexCode (){
+    private AvoidComplexCode() {
 
     }
 
-    public static void tooMuchNestedForCollect() {
-        AtomicReference<List<Integer>> nonsense;
-        nonsense = new AtomicReference<>(new ArrayList<>());
-        Consumer<AtomicReference<List<Integer>>> atomicReferenceConsumer = AvoidComplexCode::accept;
-        atomicReferenceConsumer.accept(nonsense);
+    public static Values theOldestWay(List<String> input) {
+        List<List<String>> resListINList = new ArrayList<>();
+        input.sort(new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return o1.compareTo(o2);
+            }
+        });
+
+        Long result = 0L;
+        for (int j = 0, inputSize = input.size(); j < inputSize; j++) {
+            String element = input.get(j);
+            List<String> splitted = Arrays.asList(element.split(" "));
+            List<String> partResult = new ArrayList<>();
+
+            for (int i = 0, splittedSize = splitted.size(); i < splittedSize; i++) {
+                String part = splitted.get(i);
+                if (part.trim().toUpperCase().contains("I")) {
+                    result++;
+                    partResult.add(part);
+                }
+            }
+            resListINList.add(partResult);
+
+        }
+        return new Values(result, resListINList);
     }
 
-    public static void unrolledHmpf() {
-        AtomicReference<List<Integer>> nonsense = new AtomicReference<>(new ArrayList<>());
-        accept(nonsense);
+    public static Values theOldWay(List<String> input) {
+
+        List<List<String>> resListINList = new ArrayList<>();
+        input.sort(new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return o1.compareTo(o2);
+            }
+        });
+
+        Long result = 0L;
+        for (String element : input) {
+            List<String> splitted = Arrays.asList(element.split(" "));
+            List<String> partResult = new ArrayList<>();
+
+            for (String part : splitted) {
+                if (part.trim().toUpperCase().contains("I")) {
+                    result++;
+                    partResult.add(part);
+                }
+            }
+            resListINList.add(partResult);
+
+        }
+        return new Values(result, resListINList);
     }
 
-    private static void accept(AtomicReference<List<Integer>> nonsense1) {
-        {
-            List<String> strList = Arrays.asList("Hello this is Romeo".split(" "));
-            Integer counter = Integer.valueOf(0);
-            for (String part : strList)
-                if (part.trim().equals("I")) {
-                    nonsense1.get().add(counter++);
-                }
+
+    public static Values aBitStreamLined(List<String> input) {
+
+        List<List<String>> resListINList = input.stream()
+                .sorted()
+                .map(e -> Arrays.stream(e.split(" "))
+                        .filter(e1 -> e1.trim().toUpperCase().contains("I"))
+                        .toList()).toList();
+        AtomicReference<Long> counter = new AtomicReference<>(0L);
+        resListINList.forEach(l1 ->
+                l1.forEach(e ->
+                        counter.getAndSet(counter.get() + 1))
+        );
+        return new Values(counter.get(), resListINList);
+    }
+
+    public static class Values {
+
+        final List valList;
+
+        public Values(Object... list) {
+            valList = Arrays.asList(list);
         }
-        {
-            List<String> strList = Arrays.asList("here to talk".split(" "));
-            Integer counter = Integer.valueOf(0);
-            for (String part : strList) {
-                if (part.trim().equals("I")) {
-                    nonsense1.get().add(counter++);
-                }
-            }
-        }
-        {
-            List<String> strList = Arrays.asList("I am alive".split(" "));
-            Integer counter = Integer.valueOf(0);
-            for (String part : strList) {
-                if (part.trim().equals("I")) {
-                    nonsense1.get().add(counter++);
-                }
-            }
-        }
-        {
-            List<String> strList = Arrays.asList("am I crazy".split(" "));
-            Integer counter = Integer.valueOf(0);
-            for (String part : strList) {
-                if (part.trim().equals("I")) {
-                    nonsense1.get().add(counter++);
-                }
-            }
-        }
-        {
-            List<String> strList = Arrays.asList("the walking dead".split(" "));
-            Integer counter = Integer.valueOf(0);
-            for (String part : strList) {
-                if (part.trim().equals("I")) {
-                    nonsense1.get().add(counter++);
-                }
-            }
-        }
-        {
-            List<String> strList = Arrays.asList("in that case".split(" "));
-            Integer counter = Integer.valueOf(0);
-            for (String part : strList) {
-                if (part.trim().equals("I")) {
-                    nonsense1.get().add(counter++);
-                }
-            }
-        }
-        {
-            List<String> strList = Arrays.asList("a child plays".split(" "));
-            Integer counter = Integer.valueOf(0);
-            for (String part : strList) {
-                if (part.trim().equals("I")) {
-                    nonsense1.get().add(counter++);
-                }
-            }
-        }
-        {
-            List<String> strList = Arrays.asList("the evil hurricane".split(" "));
-            Integer counter = Integer.valueOf(0);
-            for (int i = 0, strListSize = strList.size(); i < strListSize; i++) {
-                String part = strList.get(i);
-                if (part.trim().equals("I")) {
-                    nonsense1.get().add(counter++);
-                }
-            }
+
+        public List getValList() {
+            return Collections.unmodifiableList(valList);
         }
     }
 
-    public void abitStreamLined() {
-        List <String> input = Arrays.asList("the evil hurricane",
+    public static void printListList(List<List<String>> theList) {
+        theList.forEach(l1 -> {
+            System.out.println("Group-Change:");
+            l1.forEach(e -> System.out.println(e));
+        });
+    }
+
+    public static void printList(List<String> theList, String headLine) {
+        System.out.println(headLine);
+        theList.forEach(e ->
+                System.out.println(e));
+        System.out.println("--- PRINT out end ---");
+    }
+
+    public static void main(String[] arg) {
+        List<String> inputOld = Arrays.asList("the evil hurricane",
                 "a child plays",
                 "in that case",
                 "the walking dead",
                 "I am alive",
                 "Hello this is Romeo",
-                "here to talk" );
-        long result =  input.stream()
-                .sorted()
-                .map(e -> Arrays.stream(e.split(" "))
-                        .filter(e1 -> e1.trim().equals("I"))
-                        .findAny().orElse("null")).count();
+                "here to talk");
+        List<String> inputStream = Arrays.asList("the evil hurricane",
+                "a child plays",
+                "in that case",
+                "the walking dead",
+                "I am alive",
+                "Hello this is Romeo",
+                "here to talk");
+        printList(inputStream, "--- New Array before fun call ---");
+        Values values = aBitStreamLined(inputStream);
+        System.out.println("======== the new way with streams =============");
+        printList(inputStream, "The new way has NO side effects at all - after fun call");
+        long counter = (Long) values.getValList().get(0);
+        List<List<String>> theList = (List<List<String>>) values.getValList().get(1);
+        System.out.println("List counter: " + counter);
+        printListList(theList);
+        printList(inputOld, "--- Old Array before fun call ---");
+        values = theOldWay(inputOld);
+        System.out.println("======== the old way with loop =============");
+        printList(inputOld, "The old way has side effects - after fun call");
+        counter = (Long) values.getValList().get(0);
+        theList = (List<List<String>>) values.getValList().get(1);
+        System.out.println("List counter: " + counter);
+        printListList(theList);
     }
 }
